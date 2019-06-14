0a42b111911381dc256bcca20c49e0d3f9eec2f0
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-06-14 11:59:35 -0500

    updated puzzle

--- puzzling.java
+++ puzzling.java
@@ -6,5 +6,16 @@ import java.util.Collections;
 import java.util.Random;
 
 public class puzzling {
+    public ArrayList<Integer> greaterThanTen(int[] arr){
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>10){
+                numbers.add(arr[i]);
+            }            
+
+        }
+        return numbers;
+    }
+
 
 }
--- puzzlingTest.java
+++ puzzlingTest.java
@@ -4,6 +4,9 @@
 public class puzzlingTest {
 
     public static void main(String[] args) {
-        
+        puzzling puzzle = new puzzling();
+        // Puzzle number 1
+        int [] arr = {3,5,1,2,7,9,8,13,25,32};
+        System.out.println(puzzle.greaterThanTen(arr));
     }
 }
