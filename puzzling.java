277bfe29c6fad5c3d85d1276276bd85c731088d3
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-06-14 12:25:44 -0500

    "puzzle num  2 added"

--- puzzling.java
+++ puzzling.java
@@ -8,13 +8,35 @@ import java.util.Random;
 public class puzzling {
     public ArrayList<Integer> greaterThanTen(int[] arr){
         ArrayList<Integer> numbers = new ArrayList<Integer>();
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
             if (arr[i]>10){
                 numbers.add(arr[i]);
             }            
-
         }
+        System.out.println("sum for the array is: " + sum);
         return numbers;
+    }
+    public ArrayList<String> greaterThanFive(String[] names){
+        ArrayList<String> name = new ArrayList<String>();
+        Random rand = new Random();
+        // shuffling the array 20 times
+        String temp = "";
+        for (int i = 0; i < 20; i++) {
+            int randInd = rand.nextInt(names.length);
+            int randInd2 = rand.nextInt(names.length);
+            temp = names[randInd];
+            names[randInd] = names[randInd2];
+            names[randInd2] = temp;
+        }
+        for (int i = 0; i < names.length; i++) {
+            System.out.println((i+1) + ": " + names[i]);
+            if (names[i].length() > 5){
+                name.add(names[i]);
+            }            
+        }
+        return name;
     }
 
 
--- puzzlingTest.java
+++ puzzlingTest.java
@@ -6,8 +6,12 @@ public class puzzlingTest {
     public static void main(String[] args) {
         puzzling puzzle = new puzzling();
         // Puzzle number 1
+        System.out.println("Part 1");
         int [] arr = {3,5,1,2,7,9,8,13,25,32};
         System.out.println(puzzle.greaterThanTen(arr));
-        //   
+        //   Puzzle number 2
+        System.out.println("Part 2");
+        String[] names = {"Nancy","Jinichi","Fujibayashi","Momochi","Ishikawa"};
+        System.out.println("Names greater than 5: " + puzzle.greaterThanFive(names)); 
     }
 }
